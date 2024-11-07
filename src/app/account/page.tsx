f402f3d934d980da6be9895be39e9c3c55a98009
yongkaiyu <150000961+yongkaiyu@users.noreply.github.com>
2024-11-07 13:26:15 +0800

    missing useEffect added

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Account() {
     const [name, setName] = useState('');
